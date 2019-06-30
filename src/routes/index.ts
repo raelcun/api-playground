cbaadb722d8a2a7e11c803f7439db81dfc43bf4a
Dan Taylor <dt.raelcun@gmail.com>
2019-06-29 20:20:44 -0400

    added v1 route prefix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,6 @@ import Router from 'koa-router'
 import { router as adminThingRouter } from './adminMessage'
 import { router as healthcheckRouter } from './healthcheck'
 
-export const router = new Router()
+export const router = new Router({ prefix: '/v1' })
   .use(adminThingRouter.routes(), adminThingRouter.allowedMethods())
   .use(healthcheckRouter.routes(), healthcheckRouter.allowedMethods())
