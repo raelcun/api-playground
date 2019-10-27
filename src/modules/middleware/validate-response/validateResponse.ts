14a9ef92af9dcbedc6725678325c8e6c89fe2ddf
Dan Taylor <dt.raelcun@gmail.com>
2019-10-27 01:11:38 -0400

    wip finished refactoring validatedResponse

--- src/modules/middleware/validate-response/validateResponse.ts
+++ src/modules/middleware/validate-response/validateResponse.ts
@@ -1,30 +1,33 @@
 import * as t from 'io-ts'
 import { reporter } from 'io-ts-reporters'
-import { left } from 'fp-ts/lib/Either'
+import { left, fold } from 'fp-ts/lib/Either'
 import HttpStatus from 'http-status-codes'
+import { pipe } from 'fp-ts/lib/pipeable'
 import { Middleware } from '../../../types'
 import { getSystemLogger } from '../../logger'
 import { getConfig } from '../../../config'
 
 export const validateResponseInner = (shouldError: boolean) => <T>(
   type: t.Type<T, unknown>,
-): Middleware<T> => async (ctx, next) => {
-  type.decode(ctx.body).fold(
-    errors => {
-      getSystemLogger().warn('failed to validate response\n' + reporter(left(errors)))
-      if (shouldError) {
-        ctx.status = HttpStatus.INTERNAL_SERVER_ERROR
-        ctx.body = {
-          errors: reporter(left(errors)),
+): Middleware<T> => async (ctx, next) =>
+  pipe(
+    type.decode(ctx.body),
+    fold(
+      errors => {
+        getSystemLogger().warn('failed to validate response\n' + reporter(left(errors)))
+        if (shouldError) {
+          ctx.status = HttpStatus.INTERNAL_SERVER_ERROR
+          ctx.body = {
+            errors: reporter(left(errors)),
+          }
+        } else {
+          next()
         }
-      } else {
+      },
+      () => {
         next()
-      }
-    },
-    () => {
-      next()
-    },
+      },
+    ),
   )
-}
 
 export const validateResponse = validateResponseInner(!getConfig().isProduction)
